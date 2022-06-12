ffa842caf5cc74b46caec1c151cbc5592404fc79
Dee Rommes <deerommes@Dees-MacBook-Air.local>
2022-06-12 12:02:03 -0400

    added Missions table

--- src/Missions.tsx
+++ src/Missions.tsx
@@ -1,4 +1,56 @@
+import {
+  Paper,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+  TextField,
+  Toolbar,
+  Typography,
+} from '@mui/material';
+
 const Missions = () => {
-  return <div>Hello Missions</div>;
+  return (
+    <Paper sx={{ m: 2 }}>
+      <TableContainer>
+        <Toolbar sx={{ p: 2 }}>
+          <Typography
+            sx={{ flex: '1 1 100%' }}
+            variant="h6"
+            id="tableTitle"
+            component="div"
+          >
+            Missions
+          </Typography>
+          <TextField label="Filter by name" variant="outlined" />
+        </Toolbar>
+        <Table aria-label="table">
+          <TableHead>
+            <TableRow>
+              <TableCell component="th" scope="row">
+                Name
+              </TableCell>
+              <TableCell>Twitter</TableCell>
+              <TableCell>Website</TableCell>
+              <TableCell>Wikipedia</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            <TableRow>
+              <TableCell component="th" scope="row">
+                mission
+              </TableCell>
+              <TableCell>twitter</TableCell>
+              <TableCell>website</TableCell>
+              <TableCell>wikipedia</TableCell>
+            </TableRow>
+          </TableBody>
+        </Table>
+      </TableContainer>
+    </Paper>
+  );
 };
+
 export default Missions;
